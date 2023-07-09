af6a2586bf398ffed871cd2e6145e6c36225bb26
julianbischoff <julibischoff@gmail.com>
2023-07-09 15:03:47 -0600

    home completo

--- src/components/modalNuevo/index.jsx
+++ src/components/modalNuevo/index.jsx
@@ -1,22 +1,17 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import './styles.scss';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { EffectCoverflow, Pagination, Navigation } from 'swiper';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faHouse} from "@fortawesome/free-solid-svg-icons";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import Router from "../../router";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./styles.scss";
 
-import slide_image_1 from '../../assets/FsGrupo.png';
-import slide_image_2 from '../../assets/CasaGarita.jpg';
-import slide_image_3 from '../../assets/CasaH.jpg';
-import slide_image_4 from '../../assets/Oficinas1.jpg';
-import slide_image_5 from '../../assets/Psicina.jpg';
-
+import slide_image_1 from "../../assets/FsGrupo.png";
+import slide_image_2 from "../../assets/ArquitecturaEdit.jpg";
+import slide_image_3 from "../../assets/DiseñoEdit.jpg";
+import slide_image_4 from "../../assets/AdministracionEdit.jpg";
+import slide_image_5 from "../../assets/ControlEdit.jpg";
 
 const NuevoSlider = () => {
   const [isOpen, setIsOpen] = useState(true);
@@ -25,72 +20,67 @@ const NuevoSlider = () => {
     setIsOpen(false);
   };
 
-  const buttonVariants = {
-    hover: { scale: 1.1 },
-    tap: { scale: 0.9 },
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    className: "slider-container",
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [
+      {
+        breakpoint: 768, // Ajusta las configuraciones para pantallas de celular
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          arrows: false,
+        },
+      },
+    ],
   };
 
   return (
     <>
-      {isOpen ? (
-        <div className="container-modal">
-          <div className="swiper-controler">
-    
-            <div className="modal-button-container">
-              <motion.button
-                className="modal-enter-button-saltarin "
-                onClick={handleClose}
-                variants={buttonVariants}
-                whileHover="hover"
-                whileTap="tap"
-              >
-               <FontAwesomeIcon icon={faHouse} />
-              </motion.button>
+      <div className="home_container">
+        {isOpen ? (
+          <div className="container-modal">
+            <div className="swiper-controler">
+              <div className="modal-button-container">
+                <button
+                  className="modal-enter-button-saltarin"
+                  onClick={handleClose}
+                >
+                  <FontAwesomeIcon icon={faHouse} />
+                </button>
+              </div>
             </div>
+            <Slider {...settings}>
+              <div>
+                <img src={slide_image_1} alt="slide_image" />
+              </div>
+              <div>
+                <img src={slide_image_2} alt="slide_image" />
+              </div>
+              <div>
+                <img src={slide_image_3} alt="slide_image" />
+              </div>
+              <div>
+                <img src={slide_image_4} alt="slide_image" />
+              </div>
+              <div>
+                <img src={slide_image_5} alt="slide_image" />
+              </div>
+            </Slider>
           </div>
-          <Swiper
-            effect={'coverflow'}
-            grabCursor={true}
-            centeredSlides={true}
-            loop={true}
-            slidesPerView={'auto'}
-            coverflowEffect={{
-              rotate: 0,
-              stretch: 0,
-              depth: 100,
-              modifier: 2.5,
-            }}
-            pagination={{ clickable: true }}
-            navigation={{
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-              clickable: true,
-            }}
-            modules={[EffectCoverflow, Pagination, Navigation]}
-            className="swiper_container"
-          >
-            <SwiperSlide>
-              <img src={slide_image_1} alt="slide_image" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={slide_image_2} alt="slide_image" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={slide_image_3} alt="slide_image" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={slide_image_4} alt="slide_image" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={slide_image_5} alt="slide_image" />
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      ) : (
-        <Router />
-      )}
+        ) : (
+          <Router />
+        )}
+      </div>
     </>
   );
-}
+};
 
 export default NuevoSlider;
