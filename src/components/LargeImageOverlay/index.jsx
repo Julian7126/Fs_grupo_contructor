560088b22da50b888751043cf8f4ee25a6414aea
julianbischoff <julibischoff@gmail.com>
2023-12-07 19:44:34 -0600

    cambios2

--- src/components/LargeImageOverlay/index.jsx
+++ src/components/LargeImageOverlay/index.jsx
@@ -1,3 +1,5 @@
+// LargeImageOverlay.jsx
+
 import React from 'react';
 import { motion } from 'framer-motion';
 import { Image } from 'react-bootstrap';
@@ -5,32 +7,27 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./styles.scss"
 
-
 const LargeImageOverlay = ({ handleCloseImage, largeImageUrl, imageIdentifier }) => {
   return (
-    
-   
-    
-    
     <motion.div
       className="modal-overlay"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      >
+    >
       <div className="modal-content_detail">
-      <FontAwesomeIcon
+        <div className="close-icon-container">
+          <FontAwesomeIcon
             icon={faTimes}
             className="close-icon"
             onClick={handleCloseImage}
-            />
+          />
+        </div>
         <div className="large-image_detail_container">
           <Image className='large_image' src={largeImageUrl} alt={imageIdentifier} fluid />
         </div>
-      
       </div>
     </motion.div>
-   
   );
 };
 
--- src/components/modalNuevo/index.jsx
+++ src/components/modalNuevo/index.jsx
@@ -7,7 +7,7 @@ import "./styles.scss";
 
 import slide_image_1 from "../../assets/FsGrupo.png";
 import slide_image_2 from "../../assets/ArquitecturaEdit.jpg";
-import slide_image_3 from "../../assets/DisenoEdit.jpg"
+import slide_image_3 from "../../assets/DisenoEdit.jpg";
 import slide_image_4 from "../../assets/AdministracionEdit.jpg";
 import slide_image_5 from "../../assets/ControlEdit.jpg";
 
@@ -25,8 +25,8 @@ const NuevoSlider = () => {
     }
   }, []);
 
-  const handleImageDoubleClick = () => {
-    setIsOpen(false);
+  const handleImageClick = () => {
+    setIsOpen(!isOpen);
   };
 
   const settings = {
@@ -37,7 +37,7 @@ const NuevoSlider = () => {
     slidesToScroll: 1,
     className: "slider-container",
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 1500,
     pauseOnHover: false,
     responsive: [
       {
@@ -58,25 +58,25 @@ const NuevoSlider = () => {
         <div className="container-modal">
           <Slider ref={sliderRef} {...settings}>
             <div>
-              <img src={slide_image_1} alt="slide_image" onDoubleClick={handleImageDoubleClick} />
+              <img src={slide_image_1} alt="slide_image" onClick={handleImageClick} />
             </div>
             <div>
-              <img src={slide_image_2} alt="slide_image" onDoubleClick={handleImageDoubleClick} />
+              <img src={slide_image_2} alt="slide_image" onClick={handleImageClick} />
             </div>
             <div>
-              <img src={slide_image_3} alt="slide_image" onDoubleClick={handleImageDoubleClick} />
+              <img src={slide_image_3} alt="slide_image" onClick={handleImageClick} />
             </div>
             <div>
-              <img src={slide_image_4} alt="slide_image" onDoubleClick={handleImageDoubleClick} />
+              <img src={slide_image_4} alt="slide_image" onClick={handleImageClick} />
             </div>
             <div>
-              <img src={slide_image_5} alt="slide_image" onDoubleClick={handleImageDoubleClick} />
+              <img src={slide_image_5} alt="slide_image" onClick={handleImageClick} />
             </div>
           </Slider>
           {window.innerWidth > 767 ? (
-            <div className="slider-legend top-legend">Double-click to Enter</div>
+            <div className="slider-legend top-legend">Enter</div>
           ) : (
-            <div className="slider-legend bottom-legend">Ingrese con Double-click</div>
+            <div className="slider-legend bottom-legend">Enter</div>
           )}
         </div>
       ) : (
